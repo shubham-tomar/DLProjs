a9f4215cd84b586ed542b80997137a4cf3bd132f
shubham Tomar <shubham.n.h.tomar@gmail.com>
2020-08-09 13:56:27 +0530

    8/9/2020

--- DLProjs/dKeras.py
+++ DLProjs/dKeras.py
@@ -8,4 +8,13 @@ print(df.head)
 
 X = df.iloc[:,3:13]
 y = df.iloc[:,13]
+geography = pd.get_dummies(X["Geography"],drop_first=True)
+gender = pd.get_dummies(X["Gender"],drop_first=True)
+X = pd.concat([X,geography,gender],axis=1)
+X = X.drop(["Geography","Gender"],axis=1)
+# print(X.isnull().sum())
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
